ac2b178307f89034b7100d57a87c593bfc5bf4a8
Manav <patelmanav2406@gmail.com>
2024-04-11 23:47:06 +0530

    create a middleware to handle errors

--- api/controllers/authTeacher.controller.js
+++ api/controllers/authTeacher.controller.js
@@ -1,7 +1,7 @@
 import Teacher from "../models/teacher.model.js";
 import bcryptjs from 'bcryptjs';
 
-export const SignUp = async (req, res) => {
+export const SignUp = async (req, res, next) => {
     const { username, RegistrationNumber, email, gender, DOB, password, cpassword } = req.body;
     const hashedTPassword = bcryptjs.hashSync(password, 10);
     const hashedTCpassword = bcryptjs.hashSync(cpassword, 10);
@@ -10,6 +10,6 @@ export const SignUp = async (req, res) => {
         await newTeacher.save()
         res.status(201).json('Teacher Created Successfully!');
     } catch (error) {
-        res.status(500).json(error.message); 
+        next(error);
     }
 }
