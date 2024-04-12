1db8d81b023274ae775a87a50fd7da63ed37c106
Manav <patelmanav2406@gmail.com>
2024-04-13 00:54:48 +0530

    created complete signup functionality for teachers

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,9 @@ import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
+import SignInTeacher from './pages/SignInTeacher';
 import SignUp from './pages/SignUp';
+import SignUpTeacher from './pages/SignUpTeacher';
 import About from './pages/About';
 import Profile from './pages/Profile';
 
@@ -12,7 +14,9 @@ export default function App() {
   <Routes>
     <Route path = "/" element={<Home />} />
     <Route path = "/sign-in" element={<SignIn />} />
+    <Route path = "/sign-in-T" element={<SignInTeacher />} />
     <Route path = "/sign-up" element={<SignUp />} />
+    <Route path = "/sign-up-T" element={<SignUpTeacher />} />
     <Route path = "/about" element={<About />} />
     <Route path = "/profile" element={<Profile />} />
   </Routes>
