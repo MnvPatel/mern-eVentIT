8b9854b4e6f7107bb9f2b88d9e829037cdfd3228
Manav <patelmanav2406@gmail.com>
2024-04-24 01:04:21 +0530

    create get user listings api

--- api/controllers/teacher.controller.js
+++ api/controllers/teacher.controller.js
@@ -1,6 +1,7 @@
 import Teacher from "../models/teacher.model.js";
 import { errorHandler } from "../utils/error.js";
 import bcryptjs from 'bcryptjs';
+import Listing from "../models/listing.model.js"
 
 export const testt = (req,res) => {
     res.json({
@@ -33,3 +34,16 @@ export const updateTeacher = async (req, res, next) => {
         next(error)
     }
 };
+
+export const getUserListings = async (req, res, next) => {
+    if (req.teacher.id === req.params.id){
+        try {
+            const listings = await Listing.find({userRef: req.params.id});
+            res.status(200).json(listings);
+        } catch (error) {
+           next(error) 
+        }
+    }else{
+        return next(errorHandler(401, 'You can only view your own listings!'))
+    }
+}
--- api/routes/teacher.route.js
+++ api/routes/teacher.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { testt, updateTeacher } from '../controllers/teacher.controller.js';
+import { getUserListings, testt, updateTeacher } from '../controllers/teacher.controller.js';
 import { verifyToken } from '../utils/verifyTeacher.js';
 
 const router = express.Router();
 
 router.get('/testt', testt);
 router.get('/updateTeacher/:id', verifyToken, updateTeacher);
+router.get('/listings/:id', verifyToken, getUserListings);
 
 export default router;
