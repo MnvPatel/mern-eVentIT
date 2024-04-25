9b931e78c831afa8385bae1ae97cea0d36d47322
Manav <patelmanav2406@gmail.com>
2024-04-26 00:40:32 +0530

    created about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About itihsdbfjksbd</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About eVentIT</h1>
+      <p className='mb-4 text-slate-700'>eVentIT is an Online meeting platform for event hosting.</p>
+      <p className='mb-4 text-slate-700'>
+      Our mission is to help our students of VIT Bhopal University to join online events smoothly using our platform specially made for VITians.
+      </p>
+      <p className='mb-4 text-slate-700'>.....................................................................................................................................................................................................................................................................................</p>
+    </div>
   )
 }
