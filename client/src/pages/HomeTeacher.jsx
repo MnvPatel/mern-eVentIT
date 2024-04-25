1fc83a3d2bb6a3669a02a5c3194ccbf2ff2812b0
Manav <patelmanav2406@gmail.com>
2024-04-26 00:36:23 +0530

    created home page

--- client/src/pages/HomeTeacher.jsx
+++ client/src/pages/HomeTeacher.jsx
@@ -1,11 +1,33 @@
-import { useState } from 'react'
 import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
+import SwiperCore from 'swiper';
+import 'swiper/css/bundle';
+import ListingItem from '../components/ListingItem';
 
 export default function HomeTeacher() {
   const [showListingsError, setShowListingsError] = useState(false);
   const {currentUser} = useSelector(state => state.user);
-  const [userListings, setUserListings] = useState([])
+  const [userListings, setUserListings] = useState([]);
+
+  const [offerListings, setOfferListings] = useState([]);
+  SwiperCore.use([Navigation]);
+  console.log(offerListings);
+  useEffect(() => {
+    const fetchOfferListings = async () => {
+      try {
+        const res = await fetch('/api/listing/get?offer=true&limit=4');
+        const data = await res.json();
+        setOfferListings(data);
+        fetchRentListings();
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchOfferListings();
+  }, []);
 
   const handleShowListings = async() => {
     try {
@@ -39,7 +61,65 @@ export default function HomeTeacher() {
     }
   }
   return (
-    <div className='p-6 max-w-lg mx-auto'>
+    <div>
+      {/* top */}
+      <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto'>
+        <h1 className='text-slate-700 font-bold text-3xl lg:text-6xl'>
+          Find your next <span className='text-slate-500'>perfect</span>
+          <br />
+          place with ease
+        </h1>
+        <div className='text-gray-400 text-xs sm:text-sm'>
+          Sahand Estate is the best place to find your next perfect place to
+          live.
+          <br />
+          We have a wide range of properties for you to choose from.
+        </div>
+        <Link
+          to={'/search'}
+          className='text-xs sm:text-sm text-blue-800 font-bold hover:underline'
+        >
+          Let's get started...
+        </Link>
+      </div>
+
+      {/* swiper */}
+      <Swiper navigation>
+        {offerListings &&
+          offerListings.length > 0 &&
+          offerListings.map((listing) => (
+            <SwiperSlide>
+              <div
+                style={{
+                  background: `url(${listing.imageUrls[0]}) center no-repeat`,
+                  backgroundSize: 'cover',
+                }}
+                className='h-[700px]'
+                key={listing._id}
+              ></div>
+            </SwiperSlide>
+          ))}
+      </Swiper>
+
+      {/* listing results for offer, sale and rent */}
+
+      <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10'>
+        {offerListings && offerListings.length > 0 && (
+          <div className=''>
+            <div className='my-3'>
+              <h2 className='text-2xl font-semibold text-slate-600'>Recent offers</h2>
+              <Link className='text-sm text-blue-800 hover:underline' to={'/search?offer=true'}>Show more offers</Link>
+            </div>
+            <div className='flex flex-wrap gap-4'>
+              {offerListings.map((listing) => (
+                <ListingItem listing={listing} key={listing._id} />
+              ))}
+            </div>
+          </div>
+        )}
+        
+      </div>
+      <div className='p-6 max-w-lg mx-auto'>
       <Link className='bg-green-700 text-white  p-3 rounded-lg uppercase text-center hover:opacity-95' to={"/create-listing"}>
         Create Listing
       </Link>
@@ -87,6 +167,7 @@ export default function HomeTeacher() {
           ))}
         </div>
       )}
+    </div>
     </div>
   )
 }
